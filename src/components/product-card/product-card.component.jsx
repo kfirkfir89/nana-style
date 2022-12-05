5421e8efe1dc18d54d5a0170683982566a219272
kfirkfir89 <kfirkfir89@gmail.com>
2022-12-05 11:18:26 +0200

    test

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -11,7 +11,7 @@ const ProductCard = ({ product }) => {
   const { addItemToCart } = useContext(CartContext);
 
   const addProductToCart = () => addItemToCart(product);
-
+//check
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`}/>
