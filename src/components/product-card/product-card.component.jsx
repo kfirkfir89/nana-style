c3388275ddb325302b1c6abed78212ef6f273bb2
kfirkfir89 <kfirkfir89@gmail.com>
2022-12-05 11:06:28 +0200

    cartdropdown funcunality added

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,10 +1,17 @@
+import { useContext } from 'react';
+
 import './product-card.styles.scss';
 
 import Button from '../button/button.component';
+import { CartContext } from '../../contexts/cart.context';
 
 const ProductCard = ({ product }) => {
+
   const { name, price, imageUrl } = product;
-  console.log("dsacvdsacascsddddddddddddddd");
+  const { addItemToCart } = useContext(CartContext);
+
+  const addProductToCart = () => addItemToCart(product);
+
   return (
     <div className="product-card-container">
       <img src={imageUrl} alt={`${name}`}/>
@@ -12,7 +19,7 @@ const ProductCard = ({ product }) => {
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
       </div>
-      <Button buttonType='defaultBtn'>ADD TO BAG</Button>
+      <Button buttonType='defaultBtn' onClick={addProductToCart}>ADD TO BAG</Button>
     </div>
   );
 };
