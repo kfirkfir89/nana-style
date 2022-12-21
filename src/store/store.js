3686f074847c2421fd5f7a2c229a7af458520f13
kfirkfir89 <kfirkfir89@gmail.com>
2022-12-21 13:02:04 +0200

    add redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,6 +1,7 @@
 import { legacy_createStore as createStore } from "redux";
 import { compose, applyMiddleware } from "redux";
-//import logger from 'redux-logger';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from "redux-persist/lib/storage";
 
 import { rootReducer } from "./root-reducer";
 
@@ -11,17 +12,28 @@ const loggerMiddleware = (store) => (next) => (action) => {
     return next(action);
   }
 
-  console.log('type: ', action.type);
-  console.log('payload: ', action.payload);
-  console.log('currentState: ', store.getState());
+  console.log("%c TYPE: ", "color: #dc2626; font-weight: bold; background-color: #e5e7eb; border-radius: 2px; border: 2px solid #a1a1aa;", action.type);
+  console.log("%cPAYLOAD: ", "color: #dc2626; font-weight: bold; ", action.payload);
+  console.log("%cCURRENT: ", "color: #2563eb; font-weight: bold; ", store.getState());
+
 
   next(action);
 
-  console.log('next.state: ', store.getState());
-}
+  console.log("%c NEXT: ", "color: #2563eb; font-weight: bold; background-color: #e5e7eb; border-radius: 2px; border: 2px solid #22c55e;", store.getState());
+};
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user']
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store);
